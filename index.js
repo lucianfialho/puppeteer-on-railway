be51ac87bf5a5cb3c80602ed38bdfc9b691d468f
Lucian Fialho <lucianfialhobp@hotmail.com>
2023-10-17 15:54:53 -0300

    -- initial

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World!");
+});
+
+app.get("/screenshot", async (req, res) => {
+  const query = req.query.q || "puppeteer"; // Consulta padrão se não for especificada na URL
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(`https://www.google.com/search?q=${query}`);
+  await page.waitForTimeout(2000);
+  const screenshot = await page.screenshot();
+
+  await browser.close();
+
+  res.set("Content-Type", "image/png");
+  res.send(screenshot);
 });
 
 server.listen(PORT, () => {
