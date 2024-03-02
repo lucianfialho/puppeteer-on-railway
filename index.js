f90a867072f95bf66ee7f5a06fc27844c3bc56c0
Lucian Fialho <lucianfialhobp@hotmail.com>
2024-03-01 23:13:51 -0300

    -- nao sei o que esta havendo entao vou so mudar

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const generateImage = require("./lib/generateImage");
 const previewImage = require("./lib/previewImage");
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +13,6 @@ app.get("/generate-image", generateImage);
 
 app.get("/preview", previewImage);
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
