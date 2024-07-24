1ac1ba3b9c5a4ef04b281f17db11ca8ff7841235
Lucian Fialho <lucianfialhobp@hotmail.com>
2024-07-24 18:29:46 -0300

    -- criando rota que recebe todos os perfis

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const stealthPlugin = require("puppeteer-extra-plugin-stealth");
 const cheerio = require("cheerio");
 const dotenv = require("dotenv");
 const redis = require("redis");
-var cors = require("cors");
+const cors = require("cors");
+const { Cluster } = require("puppeteer-cluster");
 
 dotenv.config();
 puppeteerExtra.use(stealthPlugin());
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
 // Configurar Redis
-const redisUrl = "redis://default:yu3Cc503A29IyQdNfaVPorOUz@45.79.155.244:5600";
+const redisUrl = process.env.REDIS_URL;
 const redisClient = redis.createClient({ url: redisUrl });
 
 redisClient.on("error", (err) => {
@@ -37,138 +38,171 @@ redisClient.on("connect", () => {
 
 app.use(express.json());
 
-const getUserProfile = async (username) => {
-  try {
-    console.log(`Fetching profile for ${username}`);
+const analyzeRisk = (user) => {
+  let riskScore = 0;
 
-    // Verificar o cache do Redis
-    console.log("Checking Redis cache...");
+  if (user.vacBanned) riskScore += 50;
+  if (user.commentCheck) riskScore += 30;
+  if (user.isPrivate) riskScore += 20;
+  if (user.recentGames.length === 0) riskScore += 20;
+
+  const csgo = user.recentGames.find((game) => game.id === "730");
+  if (csgo && parseFloat(csgo.hours) > 1000) riskScore += 20;
+  if (csgo && parseFloat(csgo.hours) > 500) riskScore += 10;
+
+  if (user.friends < 50) riskScore += 10;
+  if (user.level < 10) riskScore += 10;
+
+  const riskPercentage = Math.min(100, riskScore);
+  return riskPercentage;
+};
+
+const checkCache = async (usernames) => {
+  const cachedProfiles = {};
+  for (const username of usernames) {
     try {
       const cachedProfile = await redisClient.get(username);
       if (cachedProfile) {
-        console.log(`Cache hit for ${username}`);
-        return JSON.parse(cachedProfile);
+        const parsedProfile = JSON.parse(cachedProfile);
+        cachedProfiles[username] = parsedProfile.riskScore;
       }
     } catch (redisErr) {
       console.error(`Error fetching from Redis for ${username}:`, redisErr);
     }
-
-    console.log(`Cache miss for ${username}. Fetching from Steam...`);
-    const browser = await puppeteerExtra.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-
-    const page = await browser.newPage();
-    await page.goto(`https://steamcommunity.com/profiles/${username}/`, {
-      waitUntil: "load",
-      timeout: 0,
-    });
-
-    const html = await page.content();
-    await browser.close();
-
-    const $ = cheerio.load(html);
-    const user = {};
-
-    const isPrivate = $(".profile_private_info").length > 0;
-    user.isPrivate = isPrivate;
-
-    if (isPrivate) {
-      user.profileName = $(".actual_persona_name").text().trim();
-      user.location = null;
-      user.avatar = null;
-      user.description = null;
-      user.level = null;
-      user.friends = null;
-      user.badges = null;
-      user.games = null;
-      user.screenshots = null;
-      user.artwork = null;
-      user.comments = null;
-      user.recentActivity = null;
-      user.recentGames = [];
-      user.vacBanned = null;
-    } else {
-      user.profileName = $(".actual_persona_name").text().trim();
-      user.location = $(".profile_flag").text().trim() || null;
-      user.avatar = $(".profile_avatar_frame img").attr("src");
-      user.description = $(".profile_summary").text().trim();
-      user.level = parseInt($(".friendPlayerLevelNum").text().trim(), 10);
-      user.friends = parseInt(
-        $(".profile_friend_links .profile_count_link_total").text().trim(),
-        10
-      );
-      user.badges = parseInt(
-        $(".profile_badges .profile_count_link_total").text().trim(),
-        10
-      );
-      user.games = parseInt(
-        $(".profile_item_links .profile_count_link_total")
-          .first()
-          .text()
-          .trim(),
-        10
-      );
-      user.screenshots = parseInt(
-        $(".profile_item_links .profile_count_link_total").eq(2).text().trim(),
-        10
-      );
-      user.artwork = parseInt(
-        $(".profile_item_links .profile_count_link_total").eq(3).text().trim(),
-        10
-      );
-      user.comments =
-        parseInt(
-          $("#commentthread_Profile_76561198301205885_totalcount")
-            .text()
-            .trim(),
-          10
-        ) || 0;
-      user.recentActivity = $(".recentgame_recentplaytime").text().trim();
-
-      user.recentGames = [];
-      $(".recent_game").each((i, element) => {
-        const game = {
-          id: $(element).find("a").attr("href").split("/").pop(),
-          title: $(element).find(".game_name a").text().trim(),
-          hours: $(element)
-            .find(".game_info_details")
-            .text()
-            .split(" hrs on record")[0]
-            .trim(),
-        };
-        user.recentGames.push(game);
-      });
-
-      // Se não houver jogos recentes listados, considerar o perfil como privado
-      if (user.recentGames.length === 0) {
-        user.isPrivate = true;
-      }
-
-      const banStatus = $(".profile_ban_status .profile_ban").text().trim();
-      user.vacBanned = banStatus.includes("banimento VAC");
-    }
-
-    // Armazenar no cache do Redis
-    console.log(`Caching profile for ${username}`);
-    await redisClient.set(username, JSON.stringify(user), { EX: 604800 }); // Expirar em 7 dias
-
-    return user;
-  } catch (err) {
-    console.error(`Error fetching profile for ${username}:`, err);
-    throw err;
   }
+  return cachedProfiles;
 };
 
-app.post("/getUserProfile", async (req, res) => {
-  const { username } = req.body;
+const fetchUserProfile = async ({ page, data: username }) => {
+  const user = {};
+
+  await page.goto(`https://steamcommunity.com/profiles/${username}/`, {
+    waitUntil: "load",
+    timeout: 60000,
+  });
+
+  const html = await page.content();
+  const $ = cheerio.load(html);
+
+  const isPrivate = $(".profile_private_info").length > 0;
+  user.isPrivate = isPrivate;
+
+  if (isPrivate) {
+    user.level = null;
+    user.friends = null;
+    user.recentGames = [];
+    user.vacBanned = null;
+  } else {
+    user.level = parseInt($(".friendPlayerLevelNum").text().trim(), 10) || 0;
+    user.friends =
+      parseInt(
+        $(".profile_friend_links .profile_count_link_total").text().trim(),
+        10
+      ) || 0;
+    user.recentGames = [];
+    $(".recent_game").each((i, element) => {
+      const game = {
+        id: $(element).find("a").attr("href").split("/").pop(),
+        title: $(element).find(".game_name a").text().trim(),
+        hours: $(element)
+          .find(".game_info_details")
+          .text()
+          .split(" hrs on record")[0]
+          .trim(),
+      };
+      user.recentGames.push(game);
+    });
+
+    if (user.recentGames.length === 0) {
+      user.isPrivate = true;
+    }
+
+    const banStatus = $(".profile_ban_status .profile_ban").text().trim();
+    user.vacBanned = banStatus.includes("banimento VAC");
+
+    await page.goto(
+      `https://steamcommunity.com/profiles/${username}/allcomments`,
+      {
+        waitUntil: "load",
+        timeout: 60000,
+      }
+    );
+    const commentsHtml = await page.content();
+    const $comments = cheerio.load(commentsHtml);
+
+    user.comments = [];
+    $comments(".commentthread_comment_text").each((i, element) => {
+      const commentText = $comments(element).text().trim();
+      user.comments.push(commentText);
+    });
+
+    user.commentCheck = user.comments.some((comment) =>
+      /cheater|wall|xitado|XITER|xiter|Denúncia/i.test(comment)
+    );
+  }
+
+  user.riskScore = analyzeRisk(user);
+
+  // Armazenar no cache do Redis
+  await redisClient.set(username, JSON.stringify(user), { EX: 604800 });
+
+  return { username, riskScore: user.riskScore };
+};
+
+app.post("/getUserProfiles", async (req, res) => {
+  const { usernames } = req.body;
+
+  if (!Array.isArray(usernames) || usernames.length === 0) {
+    return res
+      .status(400)
+      .json({ error: "Usernames must be a non-empty array" });
+  }
+
   try {
-    const userProfile = await getUserProfile(username);
-    res.json(userProfile);
+    const cachedProfiles = await checkCache(usernames);
+    const usernamesToFetch = usernames.filter(
+      (username) => !cachedProfiles[username]
+    );
+
+    let fetchedProfiles = {};
+    if (usernamesToFetch.length > 0) {
+      const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 3,
+        puppeteer: puppeteerExtra,
+        puppeteerOptions: {
+          headless: true,
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        },
+      });
+
+      await cluster.task(fetchUserProfile);
+
+      const fetchResults = await Promise.all(
+        usernamesToFetch.map((username) =>
+          cluster.execute({ username }).catch((err) => {
+            console.error(`Error fetching profile for ${username}:`, err);
+            return null;
+          })
+        )
+      );
+
+      await cluster.idle();
+      await cluster.close();
+
+      fetchedProfiles = fetchResults
+        .filter((result) => result !== null)
+        .reduce((acc, result) => {
+          acc[result.username] = result.riskScore;
+          return acc;
+        }, {});
+    }
+
+    const allProfiles = { ...cachedProfiles, ...fetchedProfiles };
+    res.json(allProfiles);
   } catch (error) {
-    console.error(`Failed to fetch user profile for ${username}:`, error);
-    res.status(500).json({ error: "Failed to fetch user profile" });
+    console.error(`Failed to fetch user profiles:`, error);
+    res.status(500).json({ error: "Failed to fetch user profiles" });
   }
 });
 
